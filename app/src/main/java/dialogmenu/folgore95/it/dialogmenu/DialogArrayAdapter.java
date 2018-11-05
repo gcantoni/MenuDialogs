bdc2234314ffa2b5e44300c72a45dd90a6ba9521
Simone Sestito <simone.dev.help@gmail.com>
2018-11-05 14:43:54 +0100

    DialogArrayAdapter: change List<Integer> to int[]
    Signed-off-by: Simone Sestito <simone.dev.help@gmail.com>

--- app/src/main/java/dialogmenu/folgore95/it/dialogmenu/DialogArrayAdapter.java
+++ app/src/main/java/dialogmenu/folgore95/it/dialogmenu/DialogArrayAdapter.java
@@ -8,16 +8,13 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author Cantoni Giorgio - giorgio.canto98@gmail.com on 31/10/18.
  */
 
 public class DialogArrayAdapter extends ArrayAdapter<String> {
 
-    public List<Integer> images;
+    private int[] images;
 
     /**
      * First Constructor - used in Simple Dialog, Rounded Dialog and Rounded Colored Dialog Menu
@@ -26,9 +23,9 @@ public class DialogArrayAdapter extends ArrayAdapter<String> {
      * @param items
      * @param images
      */
-    public DialogArrayAdapter(Context context, String[] items, Integer[] images) {
+    public DialogArrayAdapter(Context context, String[] items, int[] images) {
         super(context, R.layout.dialog, items);
-        this.images = Arrays.asList(images);
+        this.images = images;
     }
 
     @Override
@@ -37,9 +34,9 @@ public class DialogArrayAdapter extends ArrayAdapter<String> {
         TextView textView = view.findViewById(android.R.id.text1);
 
         if (Build.VERSION.SDK_INT >= 24) {
-            textView.setCompoundDrawablesRelativeWithIntrinsicBounds(images.get(position), 0, 0, 0);
+            textView.setCompoundDrawablesRelativeWithIntrinsicBounds(images[position], 0, 0, 0);
         } else {
-            textView.setCompoundDrawablesWithIntrinsicBounds(images.get(position), 0, 0, 0);
+            textView.setCompoundDrawablesWithIntrinsicBounds(images[position], 0, 0, 0);
         }
         textView.setCompoundDrawablePadding(
                 (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 12, getContext().getResources().getDisplayMetrics()));
